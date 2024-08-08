06b22210793ccadc1d3725fe522416bb1cb102f3
Johnathan Moreno <yon@192.168.1.10>
2024-08-08 14:47:37 -0700

    FEAT: Add player scene

---
+++ state.gd
@@ -0,0 +1,27 @@
+class_name State
+extends Node
+
+@export
+var animation_name: String
+@export
+var move_speed: float = 300
+
+# Hold a reference to the parent so that it can be controlled by the state
+var parent: Player
+
+func enter() -> void:
+	# If we include animations it would be like this.
+	# parent.animations.play(animation_name) 
+	pass
+
+func exit() -> void:
+	pass
+
+func process_input(_event: InputEvent) -> State:
+	return null
+
+func process_frame(_delta: float) -> State:						
+	return null
+
+func process_physics(_delta: float) -> State:
+	return null
