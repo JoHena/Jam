008bb2b7540dc36bfc220634ed74e3f5f257261d
Johnathan Moreno <yon@192.168.1.10>
2024-08-08 15:48:14 -0700

    FEAT: Player movement state

---
+++ Player/states/move_state.gd
@@ -0,0 +1,18 @@
+extends State
+
+@export var speed: float = 100
+@export var acceleration: float = 10
+
+
+func process_physics(_delta: float) -> State:
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+
+	parent.velocity.x = move_toward(parent.velocity.x, speed * direction.x, acceleration)
+	parent.velocity.y = move_toward(parent.velocity.y, speed * direction.y, acceleration)
+
+	parent.move_and_slide()
+	
+	if direction.x != 0:
+		parent.sprite.flip_h = direction.x > 0
+
+	return null
