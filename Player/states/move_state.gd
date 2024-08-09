01b4cf7f8f5a13811083df0538660ca4287d4287
Léo Chevalier <leocheche40@gmail.com>
2024-08-09 10:19:20 +0200

    CHANGE: variable names
    export only when needed
    const instead of var when possible
    const names in full caps
    Class names (nodes like Player or Civ) start with an uppercase letter

--- Player/states/move_state.gd
+++ Player/states/move_state.gd
@@ -1,14 +1,14 @@
 extends State
 
-@export var speed: float = 100
-@export var acceleration: float = 10
+const SPEED: float = 100
+const ACCELERATION: float = 10
 
 
 func process_physics(_delta: float) -> State:
 	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 
-	parent.velocity.x = move_toward(parent.velocity.x, speed * direction.x, acceleration)
-	parent.velocity.y = move_toward(parent.velocity.y, speed * direction.y, acceleration)
+	parent.velocity.x = move_toward(parent.velocity.x, SPEED * direction.x, ACCELERATION)
+	parent.velocity.y = move_toward(parent.velocity.y, SPEED * direction.y, ACCELERATION)
 
 	parent.move_and_slide()
 	
