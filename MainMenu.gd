eedfef721064a23551c3ec78ad7a5f672dcd5976
PLOFINBOOTS <luckylloop@gmail.com>
2024-08-12 15:22:15 +0200

    Main Menu
    Functionnal main menu

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,16 +1,20 @@
 extends Control
 
+
+func _ready():
+	MenuMusic.play_music_menu()
+	
 func _process(delta):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
-
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
-
-
-func _on_options_pressed():
-	pass # Replace with function body.
+	
 
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://UI elements/options.tscn")
