7e213925555fd4f8fd3fa65814b17161c2e040ed
Yom <genjilolic@gmail.com>
2024-08-14 16:36:04 -0700

    Merge branch 'main' of github.com:JoHena/Jam

--- Player/states/move_state.gd
+++ Player/states/move_state.gd
@@ -23,7 +23,7 @@ var IS_SHADOW: bool = false
 func process_physics(_delta: float) -> State:
 	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 
-	parent.velocity.x = move_toward(parent.velocity.x, SPEED * direction.x, ACCELERATION)
+	parent.velocity.x = move_toward(parent.velocity.x, SPEED * direction.x , ACCELERATION)
 	parent.velocity.y = move_toward(parent.velocity.y, SPEED * direction.y, ACCELERATION)
 
 	parent.move_and_slide()
